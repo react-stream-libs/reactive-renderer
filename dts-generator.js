098e2ed1599df9566ccfee81db1748462e1ecaeb
devdoomari.ubuntu.tablet <devdoomari@gmail.com>
2017-06-08 14:40:20 +0900

    disable dts-generator for now

--- dts-generator.js
+++ dts-generator.js
@@ -1,44 +1,49 @@
-var dtsGen = require('dts-generator').default;
-const path = require('path');
-const _ = require('lodash');
+console.log("dts-generator disabled for now...");
+// // var dtsGen = require('dts-generator').default;
+// import dtsGenerator from 'dts-generator';
+// import * as path from 'path';
+// import {
+//   dropRight,
+//   split,
+// } from 'lodash';
 
 
-function convertImportPath(params) {
-  let toImportFilePath = params.importedModuleId;
-  let currentFilePath = params.currentModuleId;
-  if (!toImportFilePath.startsWith('.')) {
-    return toImportFilePath;
-  }
-  if (toImportFilePath.endsWith('index')) {
-    toImportFilePath = toImportFilePath.substr(0, toImportFilePath.length - 5);
-  }
-  const currentFilePathSplit = _.split(currentFilePath, '/');
-  const currentDirPath = _.dropRight(currentFilePathSplit).join('/');
+// function convertImportPath(params) {
+//   let toImportFilePath = params.importedModuleId;
+//   const currentFilePath = params.currentModuleId;
+//   if (!toImportFilePath.startsWith('.')) {
+//     return toImportFilePath;
+//   }
+//   if (toImportFilePath.endsWith('index')) {
+//     toImportFilePath = toImportFilePath.substr(0, toImportFilePath.length - 5);
+//   }
+//   const currentFilePathSplit = split(currentFilePath, '/');
+//   const currentDirPath = dropRight(currentFilePathSplit).join('/');
 
-  toImportFilePath = toImportFilePath === '' ?
-    'reactive-renderer'
-     : path.join('reactive-renderer/lib', currentDirPath, toImportFilePath);
-  // console.error('convertImportPath:: ' + params.currentModuleId + ' || ' + params.importedModuleId + '  ::  ' + toImportFilePath);
-  return toImportFilePath;
-}
+//   toImportFilePath = toImportFilePath === '' ?
+//     'reactive-renderer'
+//      : path.join('reactive-renderer/lib', currentDirPath, toImportFilePath);
+//   console.error('convertImportPath:: ' + params.currentModuleId + ' || ' + params.importedModuleId + '  ::  ' + toImportFilePath);
+//   return toImportFilePath;
+// }
 
-function convertFilePath(params) {
-  let filePath = params.currentModuleId;
-  if (filePath === 'index') {
-    return 'reactive-renderer';
-  }
-  filePath = 'reactive-renderer/lib/' + filePath;
-  if (filePath.endsWith('index')) {
-    filePath = filePath.substr(0, filePath.length - 6);
-  }
-  // console.error('convertFilePath:: ' + params.currentModuleId + '  ::  ' + filePath);
-  return filePath;
-}
+// function convertFilePath(params) {
+//   let filePath = params.currentModuleId;
+//   if (filePath === 'index') {
+//     return 'reactive-renderer';
+//   }
+//   filePath = path.join('reactive-renderer/lib', filePath);
+//   if (filePath.endsWith('index')) {
+//     filePath = filePath.substr(0, filePath.length - 6);
+//   }
+//   console.error('convertFilePath:: ' + params.currentModuleId + '  ::  ' + filePath);
+//   return filePath;
+// }
 
-dtsGen({
-  name: 'reactive-renderer',
-  project: 'tsconfig.lib.json',
-  out: 'reactive-renderer.d.ts',
-  resolveModuleId: convertFilePath,
-  resolveModuleImport: convertImportPath,
-});
+// dtsGenerator({
+//   name: 'reactive-renderer',
+//   project: '../tsconfig.lib.json',
+//   out: 'reactive-renderer.d.ts',
+//   resolveModuleId: convertFilePath,
+//   resolveModuleImport: convertImportPath,
+// });
