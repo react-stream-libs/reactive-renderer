e6d89b22cb99d5eba6d54ded18f605ad3258d2c3
devdoomari.circleci <devdoomari@gmail.com>
2017-06-19 10:49:56 +0000

    generated from:7dba52613faa36d8cf198833c2ae1bd361ecddc7 triggered by:devdoomari [ci skip]

---
+++ lib_es6/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Reactive Renderer
+ */
+const BaseRenderer_1 = require("./BaseRenderer");
+exports.BaseRenderer = BaseRenderer_1.default;
+exports.renderChild = BaseRenderer_1.renderChild;
+exports.deleteChild = BaseRenderer_1.deleteChild;
+const createComponent_1 = require("./createComponent");
+exports.createComponent = createComponent_1.createComponent;
+const createComponentWithContext_1 = require("./createComponentWithContext");
+exports.createComponentWithContext = createComponentWithContext_1.createComponentWithContext;
+const types_1 = require("./types");
+exports.BaseBlueprint = types_1.BaseBlueprint;
