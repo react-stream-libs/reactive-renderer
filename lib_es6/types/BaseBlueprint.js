0e55cd5a9802aecd996b1bc58b300e321fa49205
devdoomari.circleci <devdoomari@gmail.com>
2017-07-14 03:05:50 +0000

    generated from:eeabd620a03fae9969e3f3b9f0ee85cd45d6a1f0 triggered by:devdoomari [ci skip]

---
+++ lib_es6/types/BaseBlueprint.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * @export
+ * @abstract
+ * @class BaseBlueprint
+ * @template PropsType
+ * @template CommonBlueprintBase
+ */
+class BaseBlueprint {
+    applyInitialProps(props, context) { }
+}
+exports.BaseBlueprint = BaseBlueprint;
